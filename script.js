5a836781a897cb6c79b43d324c56155a8d7f349e
dorzao <arcados2@protonmail.com>
2022-01-15 01:27:35 -0300

    implementados os gets e responses do java script

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const lista = document.querySelector('ul');
 const nomePersonagem = document.querySelector('#nome');
 const especiePersonagem = document.querySelector('#especie');
 const condicaoPersonagem = document.querySelector('#vivooumorto');
+
+
+puxarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            "content-type": 'application/json'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image; 
+        botao.innerHTML = "conheça o próximo Personagem"
+        nomePersonagem.innerHTML = data.name;
+    })
+}
+
+botao.onclick = puxarPersonagem;
