9b089b9027d2e8a29b5787143d95200ea4c2a427
dorzao <arcados2@protonmail.com>
2022-01-15 15:44:30 -0300

    resolvido o problema de gerar um número de 1 a 3 no javascript

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
-const lista = document.querySelector('ul');
+const lista = document.querySelector('#listinha');
 const nomePersonagem = document.querySelector('#nome');
 const especiePersonagem = document.querySelector('#especie');
 const condicaoPersonagem = document.querySelector('#vivooumorto');
 
+gerarValorAleatorio = () => {
+    return Math.floor((Math.random() + 0.7) * 2);
+        
+}
+
+
 
 puxarPersonagem = () => {
-    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+    let numeroAleatorio = gerarValorAleatorio();
+    /*if (numeroAleatorio <= 0.33) {
+        var numerim = 1;
+    } elseif (numeroAleatorio <= 0.66) {
+        var numerim = 2;
+    } else {
+        var numerim = 3;
+    }
+
+    if (numerim == 1) {
+        var alttxt = 'é o rick';
+    } elseif (numerim == 2) {
+        var alttxt = 'é o morty';
+    } else {
+        var alttxt = 'é a mulher';
+    } */
+        
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -15,9 +38,12 @@ puxarPersonagem = () => {
         }
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image; 
-        botao.innerHTML = "conheça o próximo Personagem"
-        nomePersonagem.innerHTML = data.name;
+        imagem.alt = 'teste';        
+        botao.innerHTML = "conhecer o próximo Personagem > >";
+        nomePersonagem.innerHTML = numeroAleatorio;
+        especiePersonagem.innerHTML = data.species;
+        condicaoPersonagem.innerHTML = data.status;
     })
 }
 
-botao.onclick = puxarPersonagem;
+botao.onclick = puxarPersonagem; 
