afea7e31f2dda457e6ee4418fd736fe03c731673
dorzao <arcados2@protonmail.com>
2022-01-15 00:06:40 -0300

    Criando as const do javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const lista = document.querySelector('ul');
+const nomePersonagem = document.querySelector('#nome');
+const especiePersonagem = document.querySelector('#especie');
+const condicaoPersonagem = document.querySelector('#vivooumorto');
